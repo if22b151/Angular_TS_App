2c75a0dc38dc20e5fd9e99e858b9f1496dce2a19
if22b151 <if22b151@technikum-wien.at>
2023-05-05 14:18:43 +0200

    everything done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'puzzle';
+  title = 'Login';
+  outputarr = [];
+
+  toOutput(event: any) {
+    this.outputarr = event;
+    
+  }
 }
---
+++ src/app/login/login.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+
+export class LoginComponent implements OnInit {
+  @Output() loginEvent = new EventEmitter<object>();
+  //email validation
+  email = new FormControl('', [Validators.required, Validators.email]);
+  hide = true;
+
+  
+  constructor() { }
+
+  ngOnInit(): void {
+
+  }
+
+  onSubmit(email: string, password: string, confirmPassword: string) {
+    const inputarr = [email, password, confirmPassword];
+    this.loginEvent.emit(inputarr);
+  }
+
+  getErrorMessage() {
+    //if nothing is entered
+    if (this.email.hasError('required')) {
+      return 'You must enter a value';
+    }
+    //if email is invalid
+    return this.email.hasError('email') ? 'Not a valid email' : '';
+  }
+}
